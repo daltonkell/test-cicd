a5688bd4deec2d726b0b0e4129b7195834f7294a
daltonkell <daltonkell13@gmail.com>
2020-02-11 15:05:12 +0000

    As part of release script, add compiled program

--- bin/travis_release.sh
+++ bin/travis_release.sh
@@ -5,6 +5,10 @@ git config --global user.email "build@travis-ci.com"
 git config --global user.name "Travis CI"
 
 git checkout $TRAVIS_BRANCH
+
+# add compiled binary as release
+git add prog
+git commit -m "Setting version to $GIT_TAG_VERSION"
 git tag v$GIT_TAG_VERSION -a -m "Tagging version v$GIT_TAG_VERSION"
 
 git push origin $TRAVIS_BRANCH --tags 2>&1
